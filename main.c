9dd13f6bebfd5e5dc36cd58acb7d06e8e2b86bfc
DanyelleAngelo <danyangelofsa@gmail.com>
2019-10-28 11:37:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include "libGraph/graph.h"
 
 
 int main(void){
   grafo *g;
   int resp;
-  printf("Digite 1 para matriz de adjacência e 2 para lista de adjacência:");
-  scanf("%d",&resp);
-  inicializa_grafo(&g,resp);
-  info_grafo(&g);
+  setlocale(LC_ALL, "Portuguese");
+  while(1){
+    printf("\t------- Menu -------\n");
+    printf("0 - Encerrar programa\n1 - Matriz de adjacência\n2 - Lista de adjacência\n3 - Busca em largura\n4 - Busca em Profundidade\n5 - Verificar componentes conexos\n\n");
+    printf("---------- Opção:");
+    scanf("%d",&resp);
+    if(resp==0){
+      printf("\n\n\t---------- Sessão encerrada! ----------\n");
+      break;
+    }
+    else if(resp==1 || resp==2){
+        initializeGraph(&g,resp);
+        infoGraph(&g);
+        break;
+    }else{
+      printf("Opção inválida!\n");
+    }
+    printf("\n\n\n");
+  }
+
 }
